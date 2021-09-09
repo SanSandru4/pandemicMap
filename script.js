f1d103d5c0a8dd9e18805b25cd4da466e718e658
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-09-10 00:00:15 +0300

    My 3-th commit

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const pandemicStartMap = "XX0X10010X000X010X0";
 let startMapArray = pandemicStartMap.split("");
-let totalNum = startMapArray.length;
+let totalNum = 0;
 
 startMapOut();
 total();
+endMapOut();
 
 function startMapOut() {
   let out = "";
   for (let i = 0; i < startMapArray.length; i++) {
-    out += `${startMapArray[i]} `;
+    if (startMapArray[i] == "X") {
+      out += '<div class = "ocean"></div>';
+    } else if (startMapArray[i] == "0") {
+      out += '<div class = "uninfected"></div>';
+    } else out += '<div class = "infected"></div>';
   }
-  document.querySelector(".start-map").textContent = out;
+  document.querySelector("#start-map").innerHTML = out;
 }
 
 function total() {
-  document.querySelector(".total").innerHTML =
+  for (let i = 0; i < startMapArray.length; i++) {
+    if (startMapArray[i] != "X") {
+      totalNum++;
+    }
+  }
+  document.querySelector("#total").innerHTML =
     "<h2>Total: " + totalNum + "</h2>";
+}
+
+function endMapOut() {
+  let pandemicLandMap = pandemicStartMap.split("X");
+  console.log(pandemicLandMap);
 }
